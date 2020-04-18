d1e05c4dd05f1d4b447aa7df7aab3734a07a2ffe
brendan-newlon <58187166+brendan-newlon@users.noreply.github.com>
2020-04-17 23:51:15 -0400

    Add files via upload

--- R/latest.R
+++ R/latest.R
@@ -11,23 +11,66 @@
 #' @examples
 #' latest("df")
 
+# testing:
+project_directory = "ECM"
+wd <-  paste0("C:\\Users\\brendan\\OneDrive - Center for Creative Leadership\\R scripts\\",project_directory,"\\")
+try(setwd (wd))
+
+
+
+x = "sp_users"
+
+
+# These need to be included:
+library(magrittr)
+library(stringr)
+library(tools)
+library(tidyr)
+
+
 latest <- function (x)
 {
-  x <- paste0(file.path("Env", "Versions", paste0(x, ".rds")))
+
+
+    x <- paste0(file.path("Env", "Versions", paste0(x, ".rds")))
   latest_fileName <- basename(x)
   latest_fileName <- unlist(str_split(latest_fileName, "\\.")[1])[1]
-  latest_version <- setNames(as.data.frame(list.files(file.path("Env",
-                                                                "Versions"), pattern = NULL, all.files = FALSE, full.names = FALSE,
-                                                      recursive = FALSE, ignore.case = FALSE, include.dirs = FALSE,
-                                                      no.. = FALSE)), c("fileNames")) %>% separate(fileNames,
-                                                                                                   c("fileName", "timestamp", "extension"), sep = "_|\\.") %>%
-    filter(fileName == latest_fileName) %>% top_n(n = 1,
-                                                  wt = timestamp) %>% mutate(latest_version = paste(fileName,
-                                                                                                    timestamp, sep = "_")) %>% mutate(latest_version = paste(latest_version,
-                                                                                                                                                             extension, sep = "."))
-  latest_file <- latest_version[1, 4]
+
+
+  # one-col df with list of files in the Env/Versions directory:
+    list = list.files(file.path("Env", "Versions"),
+                      pattern = NULL,
+                      all.files = FALSE,
+                      full.names = FALSE,
+                      recursive = FALSE,
+                      ignore.case = FALSE,
+                      include.dirs = FALSE,
+                      no.. = FALSE) %>%
+    as.data.frame(stringsAsFactors = FALSE) %>%
+      setNames("fileNames")
+
+    list$extension = tools::file_ext(list$fileNames) # get file extensions in a new column
+
+
+
+    for (i in seq_along(list$fileNames)){
+      list$fileNames[i] = list$fileNames[i]  %>% str_remove(paste0("\\.",list$extension[i]))
+    }
+
+    list = list %>% tidyr::separate(fileNames, c("fileName","timestamp"), sep = "_(?=[0-9]{14}$)")
+
+
+list = list %>%
+  filter(fileName == latest_fileName) %>%
+  top_n(n = 1, wt = timestamp) %>%
+  mutate(latest_version = paste0(fileName,"_", timestamp,".",extension))
+
+latest_file = list %>%
+  .[1,4]
+
+
   y <- readRDS(file.path("Env", "Versions", latest_file))
-  assign(latest_version$fileName, y, envir = .GlobalEnv)
+  assign(list$fileName[1], y, envir = .GlobalEnv)
 }
 # To restore the latest version of any R object backed up using saveVersion()
 # USE: eg. latest("df")
